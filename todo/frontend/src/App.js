7661808822a887205d9096e0b7e1a5e1746db224
AlexAlesh <titanoos67@gmail.com>
2023-02-20 02:08:02 +0300

    User
    add users

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -2,18 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';                                       
 
+import UserList from './components/User.js';
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       'users': []
     }
-
   }
+
+  componentDidMount() {
+      const users = [
+        {
+          "username": "Fox",
+          "first_name": "Денис",
+          "last_name": "Иванов",
+          "email": "foxden@mail.ru"
+        },
+        {
+          "username": "Doc",
+          "first_name": "Мария",
+          "last_name": "Конь",
+          "email": "docmmm@mail.ru"
+        }
+      ]
+      this.setState(
+          {
+            'users':users
+          }
+      )
+  }
+
   render () {
     return (
       <div>
-        Main App
+        <UserList users={this.state.users}/>
       </div>
     )
   }
