ebf514baebffdb7248a16e9952aa2557b7642db3
AlexAlesh <titanoos67@gmail.com>
2023-03-06 16:44:27 +0300

    lesson_4

--- todo/users/views.py
+++ todo/users/views.py
@@ -5,8 +5,13 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework import mixins, viewsets
 
 
-class UserModelViewSet(ModelViewSet):
+
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
