cc5f8bdbe82caf03f5cab764970f42e6fab904f8
AlexAlesh <titanoos67@gmail.com>
2023-03-09 19:12:59 +0300

    Lesson_5

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';                                       
 
+import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
 import UserList from './components/User.js';
+import BookList from './components/Book.js';
 import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'users': []
+      'users': [],
+      'books': []
     }
   }
     
@@ -23,12 +26,39 @@ class App extends React.Component {
         }
         )
       }).catch(error => console.log(error))
-        }
+        
+
+    axios.get('http://127.0.0.1:8000/api/book')
+      .then(response => {
+        const users = response.data
+           this.setState(
+         {
+             'books': books
+         }
+         )
+      }).catch(error => console.log(error))
+    }
 
   render () {
     return (
-      <div>
-        <UserList users={this.state.users}/>
+      
+      <div className="App">
+        <HashRouter>
+          <nav>
+            <ul>
+             <li>
+               <Link to='/'>Authors</Link>
+           </li>
+            <li>
+              <Link to='/books'>Books</Link>
+             </li>
+            </ul>
+          </nav>
+<       Switch>
+        <Route exact path='/users' component={() => <UserList items={this.state.users} />} />
+        <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+        </Switch>
+        </HashRouter>
       </div>
     )
   }
